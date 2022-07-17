e7efcc085c86a04955e20407472bff94a307da38
Jakub Knetl <knetl.j@gmail.com>
2022-07-17 20:54:33 +0200

    Add server URL, crednetials and choices for get.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
 import argparse
+import os
 
 
 def get(args):
-    print("getting " + args.resource_type)
+    if args.resource_type == "plan" or args.resource_type == "p":
+        print(f"getting plan {args.key} from {args.server}")
+    elif args.resource_type == "deployment" or args.resource_type == "d":
+        raise Exception("Working with deployments in not implement yet!")
+    else:
+        raise Exception("Unknown resource_type!")
 
 
 def main():
-    parser = argparse.ArgumentParser()  # todo: add description
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--server", default=os.environ.get("BAMBOO_SERVER"))
+    parser.add_argument("-u", "--user", default=os.environ.get("BAMBOO_USER"))
+    parser.add_argument("-p", "--password", default=os.environ.get("BAMBOO_PASSWORD"))
     sub_parsers = parser.add_subparsers()
 
     get_parser = sub_parsers.add_parser("get")
-    get_parser.add_argument("resource_type")
+    get_parser.add_argument("resource_type", choices=["p", "plan", "d", "deployment"])
+    get_parser.add_argument("key")
     get_parser.set_defaults(func=get)
 
     args = parser.parse_args()
